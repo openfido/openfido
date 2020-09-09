ad44954af4f53a5fd1db7c97a23fb553fd9bd837
Dane Summers <dsummersl@yahoo.com>
2020-09-09 13:38:43 -0400

    Send output to the pipeline server as the job runs ppgwrkflo-18
    Refactored the flow to use a class so that some shared state (uuid, etc)
    doesn't need to be repeated so much.

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,13 +1,11 @@
+from unittest.mock import Mock, call, patch
+from urllib.error import URLError
+
 import pytest
 
-from unittest.mock import call, patch
-
 from app.models import RunStateEnum
-from app.services import create_pipeline_run
-from app.tasks import execute_command, execute_pipeline, make_celery, update_run_status
+from app.tasks import RunExecutor, execute_pipeline, make_celery
 from roles.decorators import ROLES_KEY
-
-from .test_services import VALID_CALLBACK_INPUT
 
 
 def test_make_celery(app):
@@ -21,8 +19,18 @@ def test_make_celery(app):
 
 
 @patch("app.tasks.urllib_request.urlopen")
+def test_update_run_status_error(urlopen_mock, app):
+    urlopen_mock.side_effect = URLError("an http error")
+
+    with pytest.raises(URLError):
+        executor = RunExecutor("uuid", "run_uuid")
+        executor.update_run_status(RunStateEnum.RUNNING)
+
+
+@patch("app.tasks.urllib_request.urlopen")
 def test_update_run_status(urlopen_mock, app):
-    update_run_status("uuid", "run_uuid", RunStateEnum.RUNNING)
+    executor = RunExecutor("uuid", "run_uuid")
+    executor.update_run_status(RunStateEnum.RUNNING)
     assert urlopen_mock.call_count == 1
     request = urlopen_mock.call_args_list[0][0][0]
     assert (
@@ -34,40 +42,71 @@ def test_update_run_status(urlopen_mock, app):
 
 
 class ReturnValue:
-    def __init__(self, value):
+    def __init__(self, value, stdout="", stderr=""):
         self.returncode = value
+        self.stdout = stdout.encode("ascii")
+        self.stderr = stderr.encode("ascii")
 
 
 @patch("app.tasks.subprocess.run")
-def test_execute_command_failure(run_mock, app):
+def test_run_failure(run_mock, app):
+    executor = RunExecutor("uuid", "run_uuid")
+    executor.update_run_output = Mock()
+
     run_mock.return_value = ReturnValue(1)
 
     with pytest.raises(ValueError):
-        execute_command("a command", "/a/dir")
+        executor.run("a command", "/a/dir")
 
     run_mock.assert_called_once_with(
         ["a", "command"], cwd="/a/dir", capture_output=True
     )
+    executor.update_run_output.assert_called()
 
 
 @patch("app.tasks.subprocess.run")
-def test_execute_command(run_mock, app):
+def test_run(run_mock, app):
+    executor = RunExecutor("uuid", "run_uuid")
+    executor.update_run_output = Mock()
+
     run_mock.return_value = ReturnValue(0)
 
-    execute_command("a command", "/a/dir")
+    executor.run("a command", "/a/dir")
     run_mock.assert_called_once_with(
         ["a", "command"], cwd="/a/dir", capture_output=True
     )
+    executor.update_run_output.assert_called()
 
 
-@patch("app.tasks.update_run_status")
-@patch("app.tasks.execute_command")
+@patch("app.tasks.RunExecutor._make_request")
+def test_update_run_output(request_mock, app):
+    executor = RunExecutor("uuid", "run_uuid")
+
+    executor.update_run_output("stdout", "stderr")
+    request_mock.assert_called_once_with(
+        "console", {"std_out": "\nstdout", "std_err": "\nstderr"}
+    )
+
+    # extra calls add data
+    request_mock.reset_mock()
+    executor.update_run_output("more", "and more")
+    request_mock.assert_called_once_with(
+        "console", {"std_out": "\nstdout\nmore", "std_err": "\nstderr\nand more"}
+    )
+
+
+@patch("app.tasks.RunExecutor.update_run_output")
+@patch("app.tasks.RunExecutor.update_run_status")
+@patch("app.tasks.RunExecutor.run")
 @patch("os.path.exists")
 def test_execute_pipeline_no_openfido(
-    exists_mock, execute_command_mock, update_run_status_mock, app
+    exists_mock,
+    run_mock,
+    update_run_status_mock,
+    update_run_output_mock,
+    app,
 ):
     exists_mock.return_value = False
-    execute_command_mock.reset_mock()
 
     execute_pipeline(
         "uuid",
@@ -78,32 +117,33 @@ def test_execute_pipeline_no_openfido(
         "master",
     )
 
-    assert execute_command_mock.call_count == 3
-    assert execute_command_mock.call_args_list[0][0][0] == "docker pull python:3"
+    assert run_mock.call_count == 3
+    assert run_mock.call_args_list[0][0][0] == "docker pull python:3"
     assert (
-        execute_command_mock.call_args_list[1][0][0]
+        run_mock.call_args_list[1][0][0]
         == "git clone https://github.com/example gitrepo"
     )
-    assert execute_command_mock.call_args_list[2][0][0] == "git checkout master"
+    assert run_mock.call_args_list[2][0][0] == "git checkout master"
 
     assert update_run_status_mock.call_count == 2
-    assert update_run_status_mock.call_args_list[0] == call(
-        "uuid", "run_uuid", RunStateEnum.RUNNING
-    )
-    assert update_run_status_mock.call_args_list[1] == call(
-        "uuid", "run_uuid", RunStateEnum.FAILED
-    )
+    assert update_run_status_mock.call_args_list[0] == call(RunStateEnum.RUNNING)
+    assert update_run_status_mock.call_args_list[1] == call(RunStateEnum.FAILED)
 
 
-@patch("app.tasks.update_run_status")
-@patch("app.tasks.execute_command")
+@patch("app.tasks.RunExecutor.update_run_output")
+@patch("app.tasks.RunExecutor.update_run_status")
+@patch("app.tasks.RunExecutor.run")
 @patch("os.path.exists")
 @patch("app.tasks.urllib_request.urlretrieve")
-def test_execute_pipeline(
-    urlretrieve_mock, exists_mock, execute_command_mock, update_run_status_mock, app
+def test_execute_pipeline_urlerror(
+    retrieve_mock,
+    exists_mock,
+    run_mock,
+    update_run_status_mock,
+    update_run_output_mock,
+    app,
 ):
-    exists_mock.return_value = True
-    execute_command_mock.reset_mock()
+    update_run_status_mock.side_effect = URLError("an error")
 
     execute_pipeline(
         "uuid",
@@ -119,24 +159,88 @@ def test_execute_pipeline(
         "master",
     )
 
-    assert execute_command_mock.call_count == 6
-    assert execute_command_mock.call_args_list[0][0][0] == "docker pull python:3"
-    assert (
-        execute_command_mock.call_args_list[1][0][0]
-        == "git clone https://github.com/example gitrepo"
+    assert update_run_status_mock.call_count == 2
+    assert update_run_status_mock.call_args_list[0] == call(RunStateEnum.RUNNING)
+    assert update_run_status_mock.call_args_list[1] == call(RunStateEnum.FAILED)
+
+
+@patch("app.tasks.RunExecutor.update_run_output")
+@patch("app.tasks.RunExecutor.update_run_status")
+@patch("app.tasks.RunExecutor.run")
+@patch("os.path.exists")
+@patch("app.tasks.urllib_request.urlretrieve")
+def test_execute_pipeline_valueerror(
+    retrieve_mock,
+    exists_mock,
+    run_mock,
+    update_run_status_mock,
+    update_run_output_mock,
+    app,
+):
+    run_mock.side_effect = ValueError("an error")
+
+    execute_pipeline(
+        "uuid",
+        "run_uuid",
+        [
+            {
+                "name": "afile.pdf",
+                "url": "https://example.com",
+            }
+        ],
+        "python:3",
+        "https://github.com/example",
+        "master",
     )
-    assert execute_command_mock.call_args_list[2][0][0] == "git checkout master"
-    assert execute_command_mock.call_args_list[3][0][0] == "mkdir input"
-    assert execute_command_mock.call_args_list[4][0][0] == "mkdir output"
-    assert execute_command_mock.call_args_list[5][0][0].startswith("docker run --rm")
 
     assert update_run_status_mock.call_count == 2
-    assert update_run_status_mock.call_args_list[0] == call(
-        "uuid", "run_uuid", RunStateEnum.RUNNING
-    )
-    assert update_run_status_mock.call_args_list[1] == call(
-        "uuid", "run_uuid", RunStateEnum.COMPLETED
+    assert update_run_status_mock.call_args_list[0] == call(RunStateEnum.RUNNING)
+    assert update_run_status_mock.call_args_list[1] == call(RunStateEnum.FAILED)
+
+
+@patch("app.tasks.RunExecutor.update_run_output")
+@patch("app.tasks.RunExecutor.update_run_status")
+@patch("app.tasks.RunExecutor.run")
+@patch("os.path.exists")
+@patch("app.tasks.urllib_request.urlretrieve")
+def test_execute_pipeline(
+    retrieve_mock,
+    exists_mock,
+    run_mock,
+    update_run_status_mock,
+    update_run_output_mock,
+    app,
+):
+    exists_mock.return_value = True
+
+    execute_pipeline(
+        "uuid",
+        "run_uuid",
+        [
+            {
+                "name": "afile.pdf",
+                "url": "https://example.com",
+            }
+        ],
+        "python:3",
+        "https://github.com/example",
+        "master",
     )
 
-    assert urlretrieve_mock.call_count == 1
-    assert urlretrieve_mock.call_args_list[0][0][0] == "https://example.com"
+    assert run_mock.call_count == 6
+    assert run_mock.call_args_list[0][0][0] == "docker pull python:3"
+    assert (
+        run_mock.call_args_list[1][0][0]
+        == "git clone https://github.com/example gitrepo"
+    )
+    assert run_mock.call_args_list[2][0][0] == "git checkout master"
+    assert run_mock.call_args_list[3][0][0] == "mkdir input"
+    assert run_mock.call_args_list[4][0][0] == "mkdir output"
+    assert run_mock.call_args_list[5][0][0].startswith("docker run --rm")
+
+    assert update_run_status_mock.call_count == 2
+    assert update_run_status_mock.call_args_list[0] == call(RunStateEnum.RUNNING)
+    assert update_run_status_mock.call_args_list[1] == call(RunStateEnum.COMPLETED)
+
+    assert retrieve_mock.call_count == 1
+    assert retrieve_mock.call_args_list[0][0][0] == "https://example.com"
