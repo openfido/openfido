36aeddbdeef6e11ae779fe09d42b4742f55c7d18
peuanthinsan <peuanthinsan@berkeley.edu>
2020-09-29 11:05:52 -0700

    BASE_API_URL use environment variables before failover to hardcoded values

--- src/config/index.js
+++ src/config/index.js
@@ -2,9 +2,9 @@ const ENVIRONMENT_DEVELOPMENT = 'DEVELOPMENT';
 const ENVIRONMENT_STAGING = 'STAGING';
 const ENVIRONMENT_PRODUCTION = 'PRODUCTION';
 
-const BASE_API_URL_DEVELOPMENT = 'http://localhost:5000';
-const BASE_API_URL_STAGING = 'https://api.staging.openfido.org';
-const BASE_API_URL_PRODUCTION = 'https://api.staging.openfido.org';
+const BASE_API_URL_DEVELOPMENT = process.env.BASE_API_URL_DEVELOPMENT || 'http://localhost:5000';
+const BASE_API_URL_STAGING = process.env.BASE_API_URL_STAGING || 'https://api.staging.openfido.org';
+const BASE_API_URL_PRODUCTION = process.env.BASE_API_URL_PRODUCTION || 'https://api.openfido.org';
 
 const parseEnvironment = () => {
   if (window.location.hostname.includes('localhost')) return ENVIRONMENT_DEVELOPMENT;
