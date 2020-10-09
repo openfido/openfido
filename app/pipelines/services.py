6268e7e205113f1ab363b1d53c670cfd3012dd7e
Dane Summers <dsummersl@yahoo.com>
2020-10-09 14:22:36 -0400

    Adds create organization pipeline endpoint opfido-77

--- app/pipelines/services.py
+++ app/pipelines/services.py
@@ -1,15 +1,45 @@
-import jwt
 import requests
 from app.constants import WORKFLOW_API_TOKEN, WORKFLOW_HOSTNAME
 from application_roles.decorators import ROLES_KEY
 from flask import current_app
 from requests import HTTPError
 
+from .models import OrganizationPipeline, db
 from .queries import find_organization_pipelines
 
 
+def create_pipeline(organization_uuid, request_json):
+    """ Create a new pipeline associated with an organization. """
+    response = requests.post(
+        f"{current_app.config[WORKFLOW_HOSTNAME]}/v1/pipelines",
+        headers={
+            "Content-Type": "application/json",
+            ROLES_KEY: current_app.config[WORKFLOW_API_TOKEN],
+        },
+        json=request_json,
+    )
+
+    try:
+        json_value = response.json()
+        response.raise_for_status()
+
+        pipeline = OrganizationPipeline(
+            organization_uuid=organization_uuid,
+            pipeline_uuid=json_value["uuid"],
+        )
+        db.session.add(pipeline)
+        db.session.commit()
+
+        json_value["uuid"] = pipeline.uuid
+        return json_value
+    except ValueError as value_error:
+        raise HTTPError("Non JSON payload returned") from value_error
+    except HTTPError as http_error:
+        raise ValueError(json_value) from http_error
+
+
 def fetch_pipelines(organization_uuid):
-    """Verify that user is a member of AUTH_HOSTNAME
+    """Find OrganizationPipelines for an organization.
 
     Note: assumes that the organization_uuid has already been verified (by
     validate_organization() mixin)
@@ -35,6 +65,7 @@ def fetch_pipelines(organization_uuid):
         json_value = response.json()
         response.raise_for_status()
 
+        # TODO these uuids should be OrganizationPipeline uuids.
         return json_value
     except ValueError as value_error:
         raise HTTPError("Non JSON payload returned") from value_error
--- tests/pipelines/test_services.py
+++ tests/pipelines/test_services.py
@@ -1,10 +1,66 @@
 from unittest.mock import patch
 
+import pytest
+import responses
 from app.constants import WORKFLOW_API_TOKEN, WORKFLOW_HOSTNAME
-from app.pipelines.services import fetch_pipelines
+from app.pipelines.models import OrganizationPipeline, db
+from app.pipelines.services import create_pipeline, fetch_pipelines
 from application_roles.decorators import ROLES_KEY
+from requests import HTTPError
 
 from ..conftest import ORGANIZATION_UUID
+
+PIPELINE_JSON = {
+    "description": "a pipeline",
+    "docker_image_url": "python:3",
+    "name": "pipeline 1",
+    "repository_branch": "master",
+    "repository_ssh_url": "https://github.com/PresencePG/presence-pipeline-example.git",
+}
+
+
+@responses.activate
+def test_create_pipeline_bad_response(app):
+    responses.add(
+        responses.POST,
+        f"{app.config[WORKFLOW_HOSTNAME]}/v1/pipelines",
+        json=PIPELINE_JSON,
+        status=500,
+    )
+    with pytest.raises(ValueError):
+        create_pipeline(ORGANIZATION_UUID, PIPELINE_JSON)
+
+
+@responses.activate
+def test_create_pipeline_bad_json(app):
+    responses.add(
+        responses.POST, f"{app.config[WORKFLOW_HOSTNAME]}/v1/pipelines", body="notjson"
+    )
+    with pytest.raises(HTTPError):
+        create_pipeline(ORGANIZATION_UUID, PIPELINE_JSON)
+
+
+@responses.activate
+def test_create_pipeline(app):
+    json_response = dict(PIPELINE_JSON)
+    json_response.update(
+        {
+            "created_at": "2020-10-08T14:22:26.276242",
+            "updated_at": "2020-10-08T14:22:26.276278",
+            "uuid": "83ac3b4e9433431fbd6d21e7a56b6f0a",
+        }
+    )
+    responses.add(
+        responses.POST,
+        f"{app.config[WORKFLOW_HOSTNAME]}/v1/pipelines",
+        json=json_response,
+    )
+    created_pipeline = create_pipeline(ORGANIZATION_UUID, PIPELINE_JSON)
+    pipeline = OrganizationPipeline.query.order_by(
+        OrganizationPipeline.id.desc()
+    ).first()
+    json_response["uuid"] = pipeline.uuid
+    assert created_pipeline == json_response
 
 
 @patch("app.pipelines.services.requests.post")
