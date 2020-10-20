c2334e67700d6e99583e32b5fe8ebfed3da3c248
peuanthinsan <peuanthinsan@berkeley.edu>
2020-10-20 09:32:50 -0700

    composeWithDevTools - development only

--- src/config/store.js
+++ src/config/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { load, save } from 'redux-localstorage-simple';
 import { logger } from 'redux-logger';
@@ -22,10 +22,13 @@ const middlewares = [
   save(localStorageSaveConfig),
 ];
 
+let composeMiddleware = compose;
+
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
+  composeMiddleware = composeWithDevTools;
 }
 
-const store = composeWithDevTools(applyMiddleware(...middlewares))(createStore)(reducers, load(localStorageLoadConfig));
+const store = composeMiddleware(applyMiddleware(...middlewares))(createStore)(reducers, load(localStorageLoadConfig));
 
 export default store;
