bc020069beae1c4a8c2acd06f6c0459486104d03
Dane Summers <dsummersl@yahoo.com>
2020-11-02 13:46:06 -0500

    Removes check bucket.
    This requires the AWS S3 'list buckets' (all buckets) which is
    considered pretty generous in terms of permissions and also permission
    to create a bucket (which we wouldn't want to have in production). We'll
    need to update our local dev services.

--- tests/test_blob_utils.py
+++ tests/test_blob_utils.py
@@ -1,8 +1,6 @@
 from datetime import datetime
 from unittest.mock import patch
 
-from botocore.stub import Stubber
-
 import blob_utils
 
 from blob_utils.constants import (
@@ -21,28 +19,3 @@ def test_get_s3(client_mock, app):
 
     assert blob_utils.get_s3() is not None
     client_mock.assert_called()
-
-
-def test_verify_bucket(s3_client, app):
-    """Tests verifying s3 bucket helper."""
-    app.config[S3_ENDPOINT_URL] = "http://example.com"
-    app.config[S3_REGION_NAME] = "a-region"
-    app.config[S3_BUCKET] = "missing"
-
-    mock_response = {
-        "Owner": {"ID": "foo", "DisplayName": "bar"},
-        "Buckets": [{"CreationDate": datetime.now(), "Name": "baz"}],
-    }
-    mock_create_response = {
-        "Location": "quix",
-    }
-
-    with Stubber(s3_client) as stubber:
-        stubber.add_response("list_buckets", mock_response, {})
-        stubber.add_response(
-            "create_bucket",
-            mock_create_response,
-            {"ACL": "private", "Bucket": "missing"},
-        )
-
-        blob_utils.verify_bucket(stubber.client)
