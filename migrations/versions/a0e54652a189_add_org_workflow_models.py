446aaf9fb5959f13d2fe22ebf0f43662ee88cee3
Nick Polkowske <nick.polkowske@presencepg.com>
2020-11-10 17:25:47 -0800

    fix revision count

--- migrations/versions/a0e54652a189_add_org_workflow_models.py
+++ migrations/versions/a0e54652a189_add_org_workflow_models.py
@@ -1,7 +1,7 @@
 """add_org_workflow_models
 
 Revision ID: a0e54652a189
-Revises: e53a1b7d4c64
+Revises: cffe83c29d00
 Create Date: 2020-11-10 16:15:02.067607
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'a0e54652a189'
-down_revision = 'e53a1b7d4c64'
+down_revision = 'cffe83c29d00'
 branch_labels = None
 depends_on = None
 
