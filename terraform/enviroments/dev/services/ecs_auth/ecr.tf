5dcf420efba83d1cea2ee2521c9ca8a658243a67
Zablah <miguel12979@gmail.com>
2020-11-25 11:55:11 -0600

    [DELETE] Dev enviroment

--- terraform/enviroments/dev/services/ecs_auth/ecr.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ecr_repository" "ecr" {
-  name                 = "${var.client}/${var.environment}-${var.ecs_name}"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
--- terraform/enviroments/dev/services/front_end/variables.tf
+++
@@ -1,8 +0,0 @@
-// MakeFile variable
-variable "environment" { type = string }
-
-// Common variables
-variable "aws_profile" { type = string }
-variable "aws_region" { type = string }
-variable "client" { type = string }
-variable "aws_tags" { type = map(string) }
