7dd68d73de27beb5b8b00495c4a26663047e6c7b
Anna Peery <avpeery@gmail.com>
2021-01-19 15:20:49 -0800

    Fixed issue with timeseries chart display

--- docker/openfido-client/src/util/charts.js
+++ docker/openfido-client/src/util/charts.js
@@ -10,7 +10,7 @@ import {
 export const getGraphInterval = (totalPoints) => {
   const numberOfPoints = parseInt(totalPoints, 10);
 
-  return numberOfPoints > 10 ? Math.ceil(numberOfPoints / 8.0) : 1; // good for 1-8 ticks - 836px cartesian grid
+  return numberOfPoints > 8 ? Math.ceil(numberOfPoints / 8.0) : 'preserveStartEnd'; // good for 1-8 ticks - 836px cartesian grid
 };
 
 const DATE_FORMATS = [
