50081974dec391fa9ad081f12fff0372427703af
David P. Chassin <dchassin@slac.stanford.edu>
2021-02-03 06:04:25 -0800

    Create start.sh

---
+++ docker/start.sh
@@ -0,0 +1,1 @@
+docker run --rm -v /tmp:/tmp -v /var/run/docker.sock:/var/run/docker.sock -p 127.0.0.1:5001:5001 -p 127.0.0.1:5002:5002 -p 127.0.0.1:5003:5003 -p 127.0.0.1:9000:9000 -p 127.0.0.1:3000:3000 openfido/openfido
