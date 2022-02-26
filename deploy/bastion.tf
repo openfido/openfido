152a83f330d99627af7bc957b052fd61b65456bc
Catzzz <chuneleu@gmail.com>
2022-02-25 21:08:57 -0800

    create bastion instance profile

--- deploy/bastion.tf
+++ deploy/bastion.tf
@@ -10,13 +10,38 @@ data "aws_ami" "amazon_linux" {
   owners = ["amazon"]
 }
 
+
+
+resource "aws_iam_role" "bastion" {
+  name               = "${local.prefix}-bastion"
+  assume_role_policy = file("./templates/bastion/instance-profile-policy.json")
+
+  tags = local.common_tags
+}
+
+resource "aws_iam_role_policy_attachment" "bastion_attach_policy" {
+  role       = aws_iam_role.bastion.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_instance_profile" "bastion" {
+  name = "${local.prefix}-bastion-instance-profile"
+  role = aws_iam_role.bastion.name
+}
+
+
+
 # create aws ec2 instance from data 
 resource "aws_instance" "bastion" {
 
-  ami           = data.aws_ami.amazon_linux.id
+  ami = data.aws_ami.amazon_linux.id
+
   instance_type = "t2.micro"
 
+
+
   user_data = file("./templates/bastion/user-data.sh")
+  iam_instance_profile = aws_iam_instance_profile.bastion.name
 
   tags = merge(
     local.common_tags,
@@ -25,3 +50,6 @@ resource "aws_instance" "bastion" {
 
 
 }
+
+
+
