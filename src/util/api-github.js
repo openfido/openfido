814cd7295d69bfc05370a8bf6a07678c08070a7c
Thistleman <codes5096@gmail.com>
2022-03-10 11:37:37 -0800

    Updated repo search paramaters for standardization

--- src/util/api-github.js
+++ src/util/api-github.js
@@ -4,17 +4,8 @@ const axios = require('axios');
 
 /// // END OF IMPORTS /////
 
-// Create the .env file in the repo's root to store the Github APP secret so it does not get exposed on github.
-// Create your own Github Oauth app for testing/using your own secret. Replace with Org Oauth App for deployment.
-
-const GHUB_TOKEN_DEVELOPMENT = process.env.DEV_GAPP_SECRET || 'UPDATE ENV';
-const GHUB_TOKEN_STAGING = process.env.GHUB_API_SECRET || 'UPDATE ENV';
-const GHUB_TOKEN_PRODUCTION = process.env.GHUB_API_SECRET || 'UPDATE ENV';
-
-const GHUB_CLIENT_ID = process.env.GHUB_CLIENT_ID || 'UPDATE ENV';
-
 // api scrape within openfido for all pipeline repositories
-const potentialPipelines = `https://api.github.com/search/repositories?q=${encodeURIComponent('org:openfido pipeline settings in:readme')}`;
+const potentialPipelines = `https://api.github.com/search/repositories?q=${encodeURIComponent('org:openfido Pipeline status in:readme')}`;
 
 const gitApi = {
 
